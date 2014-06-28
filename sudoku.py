fb16f3804baaf8bee73e7a8ab8edd9661839af3d
Peter Seibel <peter@gigamonkeys.com>
2014-06-28 13:54:16 -0700

    Basic structure of a search-based solve function.

---
+++ sudoku.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#
+# Given a board with some squares filled in, return a board completely
+# filled in or None if there is no solution.
+#
+def solve(board):
+    s = empty_square(board)
+    if s is None:
+        return board
+    else:
+        for d in possible_digits(board, s):
+            solution = solve(assign(board, s, d))
+            if solution: return solution
