7fc20ea022e3fa39cd5b067fbedf045feefe05b5
Peter Seibel <peter@gigamonkeys.com>
2014-06-28 14:58:07 -0700

    Prepare for our list-based board representation.

--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+# If we use a 81-element list as our board, these give us lists of
+# lists of indices of various groupings of squares.
+rows  = [ list(range(r*9, (r+1)*9)) for r in range(9) ]
+cols  = [ list(range(c, 81, 9)) for c in range(9) ]
+boxes = [ [ r*9 + c for r in range(x, x+3) for c in range(y, y+3) ] for x in (0, 3, 6) for y in (0, 3, 6) ]
+
 #
 # Given a board with some squares filled in, return a board completely
 # filled in or None if there is no solution.
