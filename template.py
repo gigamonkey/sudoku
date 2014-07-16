bb14d9e1aaf8a977369aefe1c97f89e9079ee56e
Peter Seibel <pseibel@twitter.com>
2014-07-16 13:57:23 -0700

    Longer variable names.

--- template.py
+++ template.py
@@ -9,29 +9,29 @@ from sudoku import digits, main
 #
 # Recursive depth first search
 #
-def search(b):
-    if b is None or solved(b): return b
+def search(board):
+    if board is None or solved(board): return board
 
-    s = empty_square(b)
-    for d in possible_digits(b, s):
-        solution = search(assign(b, s, d))
+    square = empty_square(board)
+    for digit in possible_digits(board, square):
+        solution = search(assign(board, square, digit))
         if solution: return solution
 
     return None
 
-def solved(b):
+def solved(board):
     "Returns true if the given board is already solved."
     pass
 
-def empty_square(b):
+def empty_square(board):
     "Return an empty square to try to fill."
     pass
 
-def possible_digits(b, s):
+def possible_digits(board, square):
     "Return list of digits to try putting in square s."
     pass
 
-def assign(b, s, d):
+def assign(board, square, digit):
     "Return new board with digit d in square s."
     pass
 
